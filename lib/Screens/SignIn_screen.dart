4cb811a67033a8ac4ee940bf6aa39903bb9d9fbd
Tommybarral <isamutom27@hotmail.com>
2022-03-08 12:44:24 +0100

    main page

---
+++ lib/Screens/SignIn_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Sign In there'),),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -25,9 +26,60 @@ class MyApp extends StatelessWidget {
 
 class SignInUpScreen extends StatelessWidget {
 
+  Widget authentificationButton(Color buttonColor, String title, Color textColor) {
+    return Container(
+      height: 80,
+      width: double.infinity,
+      padding: const EdgeInsets.only(top: 25, left: 24, right: 24),
+      child: RaisedButton(
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+        color: buttonColor,
+        onPressed: () {},
+        child: Text(title, style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: textColor,),),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&w=1000&q=80'),
+                    fit: BoxFit.fill),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 6,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 60, left: 25),
+                  child: Column(
+                    children: [
+                    Text('Hello', style: TextStyle(fontSize: 55, fontWeight: FontWeight.bold, color: Colors.white),),
+                    Text('Lorem ipsum dolor sit amet', style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic, color: Colors.white),),
+                    ],
+                  ),
+                ),),
+              Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      authentificationButton(Colors.indigo, 'Sign In', Colors.white),
+                      authentificationButton(Colors.white, 'Sign Up', Colors.blueAccent),
+                ],
+              )),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
